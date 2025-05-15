e916c272fe4bb7477353e1e39ae929b75289deac
Akas_Datta <012akasdatta@gmail.com>
2025-05-16 03:41:18 +0600

    added react icon and other

--- src/AllPages/Pages/LoginPage/LoginPage.jsx
+++ src/AllPages/Pages/LoginPage/LoginPage.jsx
@@ -1,87 +1,72 @@
+import { AiFillAlipayCircle } from "react-icons/ai";
 import companyLogo from "../../../assets/CompanyLogo.png";
 import loginImage from "../../../assets/LoginImage.png";
+import { MdOutlineEmail } from "react-icons/md";
+import { PiLockKeyLight } from "react-icons/pi";
+
 
 const LoginPage = () => {
     return (
-        <div className="">
-            <div className=" items-center justify-center px-6 py-8 mx-auto">
-                <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
-                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                            Sign in to your account
-                            </h1>
-                            <form className="space-y-4 md:space-y-6" action="#">
-                            <div>
-                                <label
-                                htmlFor="email"
-                                className="block mb-2 text-sm font-medium text-gray-900"
-                                >
-                                Your email
-                                </label>
-                                <input
-                                type="email"
-                                name="email"
-                                id="email"
-                                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                                placeholder="name@company.com"
-                                required
-                                />
-                            </div>
-                            <div>
-                                <label
-                                htmlFor="password"
-                                className="block mb-2 text-sm font-medium text-gray-900"
-                                >
-                                Password
-                                </label>
-                                <input
-                                type="password"
-                                name="password"
-                                id="password"
-                                placeholder="••••••••"
-                                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                                required
-                                />
-                            </div>
-                            <div className="flex items-center justify-between">
-                                <div className="flex items-start">
-                                <div className="flex items-center h-5">
-                                    <input
-                                    id="remember"
-                                    type="checkbox"
-                                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
-                                    />
-                                </div>
-                                <div className="ml-3 text-sm">
-                                    <label htmlFor="remember" className="text-gray-500">
-                                    Remember me
-                                    </label>
-                                </div>
-                                </div>
-                                <a href="#" className="text-sm font-medium text-primary-600 hover:underline">
-                                Forgot password?
-                                </a>
-                            </div>
-                            <button
-                                type="submit"
-                                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                            >
-                                Sign in
-                            </button>
-                            <p className="text-sm font-light text-gray-500">
-                                Don’t have an account yet?{' '}
-                                <a href="#" className="font-medium text-primary-600 hover:underline">
-                                Sign up
-                                </a>
-                            </p>
-                            </form>
-                        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 max-w-screen-2xl mx-auto py-10">
+        <div className="px-6 lg:px-10">
+            <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                    <label
+                        htmlFor="Email"
+                        className="block mb-2 text-md font-medium text-white"
+                        >
+                        Email
+                    </label>
+                    <div className="relative">
+                        <span className="absolute inset-y-0 left-0 pl-5 flex items-center text-gray-400">
+                            <MdOutlineEmail />
+                        </span>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            className="pl-10 bg-[#222742] border border-gray-700 text-gray-200 rounded-full focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 placeholder:text-gray-200"
+                            placeholder="E-mail"
+                            required
+                        />
+                    </div>
                 </div>
-            </div>
-            <div>
-               <img src={loginImage} alt="" />
-            </div>
-
+                <div>
+                    <label
+                        htmlFor="Heslo"
+                        className="block mb-2 text-md font-medium text-white"
+                        >
+                        Heslo
+                    </label>
+                    <div className="relative">
+                        <span className="absolute inset-y-0 left-0 pl-5 flex items-center text-gray-400">
+                            <PiLockKeyLight />
+                        </span>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            className="pl-10 bg-[#222742] border border-gray-700 text-gray-200 rounded-full focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 placeholder:text-gray-200"
+                            placeholder="Heslo"
+                            required
+                        />
+                    </div>
+                        <p className="text-sm font-light text-gray-200 py-3 text-end underline">
+                            Zabudnuté heslo?
+                        </p>
+                </div>
+            <button
+                type="submit"
+                className="cursor-pointer w-full text-white bg-[#D13D37] hover:bg-red-800 duration-500 font-medium rounded-full text-sm px-5 py-2.5 text-center"
+            >
+                Prihlásiť sa
+            </button>
+          
+            </form>
+        </div>
+        <div>
+            <img src={loginImage} alt="" />
+        </div>
         </div>
     );
 };
