ea5f8a4602f4404a553828febe946d644643cdd7
Akas_Datta <012akasdatta@gmail.com>
2025-05-16 01:47:47 +0600

    added home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './index.css';
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { router } from './Routes/Router';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
