9fd7a5df5f52e89b91ce93ee9e04c201571a17ad
Akas_Datta <012akasdatta@gmail.com>
2025-05-16 04:03:45 +0600

    login page done

--- src/AllPages/Pages/LoginPage/LoginPage.jsx
+++ src/AllPages/Pages/LoginPage/LoginPage.jsx
@@ -7,66 +7,69 @@ import { PiLockKeyLight } from "react-icons/pi";
 
 const LoginPage = () => {
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 max-w-screen-2xl mx-auto py-10">
-        <div className="px-6 lg:px-10">
-            <form className="space-y-4 md:space-y-6" action="#">
-                <div>
-                    <label
-                        htmlFor="Email"
-                        className="block mb-2 text-md font-medium text-white"
-                        >
-                        Email
-                    </label>
-                    <div className="relative">
-                        <span className="absolute inset-y-0 left-0 pl-5 flex items-center text-gray-400">
-                            <MdOutlineEmail />
-                        </span>
-                        <input
-                            type="email"
-                            name="email"
-                            id="email"
-                            className="pl-10 bg-[#222742] border border-gray-700 text-gray-200 rounded-full focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 placeholder:text-gray-200"
-                            placeholder="E-mail"
-                            required
-                        />
-                    </div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 items-center justify-center max-w-screen-2xl mx-auto px-6 py-10">
+            <div className="px-6 lg:px-10">
+                <div className="object-cover pb-4">
+                    <img src={companyLogo} alt="" />
                 </div>
-                <div>
-                    <label
-                        htmlFor="Heslo"
-                        className="block mb-2 text-md font-medium text-white"
-                        >
-                        Heslo
-                    </label>
-                    <div className="relative">
-                        <span className="absolute inset-y-0 left-0 pl-5 flex items-center text-gray-400">
-                            <PiLockKeyLight />
-                        </span>
-                        <input
-                            type="password"
-                            name="password"
-                            id="password"
-                            className="pl-10 bg-[#222742] border border-gray-700 text-gray-200 rounded-full focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 placeholder:text-gray-200"
-                            placeholder="Heslo"
-                            required
-                        />
+                <form className="space-y-4 md:space-y-6" action="#">
+                    <div>
+                        <label
+                            htmlFor="Email"
+                            className="block mb-2 text-md font-medium text-white"
+                            >
+                            Email
+                        </label>
+                        <div className="relative">
+                            <span className="absolute inset-y-0 left-0 pl-5 flex items-center text-gray-400">
+                                <MdOutlineEmail />
+                            </span>
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                className="pl-10 bg-[#222742] border border-gray-700 text-gray-200 rounded-full focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 placeholder:text-gray-200"
+                                placeholder="E-mail"
+                                required
+                            />
+                        </div>
                     </div>
-                        <p className="text-sm font-light text-gray-200 py-3 text-end underline">
-                            Zabudnuté heslo?
-                        </p>
-                </div>
-            <button
-                type="submit"
-                className="cursor-pointer w-full text-white bg-[#D13D37] hover:bg-red-800 duration-500 font-medium rounded-full text-sm px-5 py-2.5 text-center"
-            >
-                Prihlásiť sa
-            </button>
-          
-            </form>
-        </div>
-        <div>
-            <img src={loginImage} alt="" />
-        </div>
+                    <div>
+                        <label
+                            htmlFor="Heslo"
+                            className="block mb-2 text-md font-medium text-white"
+                            >
+                            Heslo
+                        </label>
+                        <div className="relative">
+                            <span className="absolute inset-y-0 left-0 pl-5 flex items-center text-gray-400">
+                                <PiLockKeyLight />
+                            </span>
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                className="pl-10 bg-[#222742] border border-gray-700 text-gray-200 rounded-full focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 placeholder:text-gray-200"
+                                placeholder="Heslo"
+                                required
+                            />
+                        </div>
+                            <p className="text-sm font-light text-gray-200 py-3 text-end underline">
+                                Zabudnuté heslo?
+                            </p>
+                    </div>
+                <button
+                    type="submit"
+                    className="cursor-pointer w-full text-white bg-[#D13D37] hover:bg-red-800 duration-500 font-medium rounded-full text-sm px-5 py-2.5 text-center"
+                >
+                    Prihlásiť sa
+                </button>
+            
+                </form>
+            </div>
+            <div>
+            <img src={loginImage} className="w-full h-auto max-h-[650px] object-contain" alt="" />
+            </div>
         </div>
     );
 };
